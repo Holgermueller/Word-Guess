4cc0fdaa7f3a118f38bf30b09ed3b5567aaa9885
Holger Mueller <9jholger@gmail.com>
2018-04-26 15:42:53 -0500

    letter.js complete?

--- letter.js
+++ letter.js
@@ -1,17 +1,32 @@
 'use strict';
 
-const Letter = function(letters) {
+const Letter = function(character, correct) {
     //create string value to store underlying character for letter
-    this.letters = letters;
-    //create boolean value to store if letter has been guessed yet
-    this.correct = true;
-    //create function to return underlying character if guessed or underscore if not guessed
+    this.character = character;
 
-    //create function that takes character as argument and checks it against underlying character and updates...
+    //create boolean value to store if letter has been guessed yet
+    this.correct = false;
+
+    //create function to return underlying character if guessed or underscore if not guessed
+    this.returnCharacter = function(){
+        let userGuess = event.key;
+        if (userGuess === this.character) {
+            return letters;
+        } else {
+            return '_';
+        }
+    }
+
+    //create function that takes a character as argument and checks it against underlying character and updates...
     //stored boolean value to true if letter is guessed correctly
+    this.characterChecker = function(character){
+        if (character === this.character) {
+            this.correct = true;
+        } else {
+            this.correct = false;
+        }
+    }
 
 }
-
-//console.log(Letter);
 
 module.exports = Letter;
